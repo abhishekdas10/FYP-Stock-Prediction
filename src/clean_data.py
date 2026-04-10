03e9e12c11c0bdae8ffc2ed9ef36dff38461f693
Abhishek Baniya <abhishekbaniya@Abhisheks-MacBook-Air.local>
2026-04-10 01:13:29 +0100

    Modify clean_data.py

--- src/clean_data.py
+++ src/clean_data.py
@@ -6,22 +6,22 @@ df = pd.read_csv("data/raw/gc_f_gold_futures.csv")
 print("Before cleaning:")
 print(df.head())
 
-# 🔹 Fix column names (flatten if needed)
+# Fix column names (flatten if needed)
 df.columns = ["Date", "Close", "High", "Low", "Open", "Volume"]
 
-# 🔹 Remove rows where Date is missing
+# Remove rows where Date is missing
 df = df.dropna(subset=["Date"])
 
-# 🔹 Convert numeric columns properly
+# Convert numeric columns properly
 cols = ["Close", "High", "Low", "Open", "Volume"]
 
 for col in cols:
     df[col] = pd.to_numeric(df[col], errors="coerce")
 
-# 🔹 Drop any remaining NaNs
+# Drop any remaining NaNs
 df = df.dropna()
 
-# 🔹 Convert Date to datetime
+# Convert Date to datetime
 df["Date"] = pd.to_datetime(df["Date"])
 
 print("\nAfter cleaning:")
